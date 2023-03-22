d7e083d917fe0e13cdbe161df3a41eafe16f2f73
root <b.wallace@salesforce.com>
2023-03-22 15:33:25 +0000

    generation function and smoke test

--- sd_model.py
+++ sd_model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 import torchvision
+from PIL import Image
 from distributed import (
     get_world_size,
     get_rank,
@@ -20,32 +21,52 @@ from distributed import (
 import slip_models
 ####
 import transformers
+from tqdm import tqdm
 # import clip
 from diffusers import AutoencoderKL, UNet2DConditionModel, DDPMScheduler
 
 class SDModel(nn.Module):
+    """
+    This is very similar to diffusers StableDiffusionPipeline
+    but more minimal and modular
+    """
     def __init__(self,
-                model_config_name="CompVis/stable-diffusion-v1-4"):
+                model_config_name="CompVis/stable-diffusion-v1-4",
+                latent_scale = 0.18215,
+                pretrained_unet=False
+                ):
         super().__init__()
         # print("TODO: Freeze VAE + Text encoder")
-        print("TODO: Does Unet need better init?")
+        master_print("TODO: Does Unet need better init?")
         self.vae = AutoencoderKL.from_pretrained(model_config_name, 
                                             subfolder="vae")
         self.text_encoder = slip_models.CLIPTextPretrained()
         self.vae.requires_grad_(False)
         self.text_encoder.requires_grad_(False)
-        
-        unet_cfg = UNet2DConditionModel.load_config(model_config_name,
-                                                    subfolder="unet") 
-        self.unet = UNet2DConditionModel.from_config(unet_cfg)
+
+        if pretrained_unet:
+            self.unet = UNet2DConditionModel.from_pretrained(model_config_name,
+                                                             subfolder='unet')
+        else:
+            unet_cfg = UNet2DConditionModel.load_config(model_config_name,
+                                                        subfolder="unet") 
+            self.unet = UNet2DConditionModel.from_config(unet_cfg)
         
         self.scheduler = DDPMScheduler.from_pretrained(model_config_name,
                                               subfolder='scheduler')
+        self.latent_scale = latent_scale
+
+        # Not needed for training but for generation
+        self.tokenizer = transformers.CLIPTokenizer.from_pretrained(
+                model_config_name,
+            subfolder="tokenizer",
+            revision=None
+            )
         
     def forward(self, img, txt):
         encoder_hidden_states = self.text_encoder(txt)
         # Not sure if scaling is right
-        latents = self.vae.encode(img).latent_dist.sample() * 0.18215
+        latents = self.vae.encode(img).latent_dist.sample() * self.latent_scale
 
         noise = torch.randn_like(latents)
         timesteps = torch.randint(0, self.scheduler.num_train_timesteps,
@@ -62,3 +83,70 @@ class SDModel(nn.Module):
         loss = F.mse_loss(noise, noise_pred, reduction='mean')
         return loss
 
+    
+    def generate(self, prompt, batch_size=1,
+                h=512, w=512, T=50, gs=7.5):
+        device = self.unet.device
+        # modeling off of https://github.com/huggingface/diffusers/blob/main/src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py#L604
+        
+        
+        # Could condense
+        cond_tokenized_inputs = self.tokenizer(
+                                      prompt,
+                                      padding="max_length",
+                                      max_length=self.tokenizer.model_max_length,
+                                      truncation=True,
+                                      return_tensors='pt').input_ids.to(device)
+        encoder_hidden_states_C = self.text_encoder(cond_tokenized_inputs)
+        
+        uncond_tokenized_inputs = self.tokenizer(
+                                      prompt,
+                                      padding="max_length",
+                                      max_length=self.tokenizer.model_max_length,
+                                      truncation=True,
+                                      return_tensors='pt').input_ids.to(device)
+        encoder_hidden_states_UC = self.text_encoder(uncond_tokenized_inputs)
+        
+        encoder_hidden_states = torch.cat([encoder_hidden_states_UC,
+                                           encoder_hidden_states_C])
+        
+        # Prepare timesteps
+        self.scheduler.set_timesteps(T, device=device)
+        timesteps = self.scheduler.timesteps
+        
+        # Latents
+        # Hardcoding scale factor for now
+        latents = torch.randn((batch_size, 4, h//8, w//8),
+                              device=device, dtype=self.unet.dtype)
+        latents = latents * self.scheduler.init_noise_sigma # 1 for DDPM
+        
+        # Denoising
+        for i,t in tqdm(enumerate(timesteps)):
+            latent_model_input = torch.cat([latents] * 2) # clf-free guidance
+            # No change below for DDPM scheduler
+            latent_model_input = self.scheduler.scale_model_input(
+                                        latent_model_input,
+                                         t
+            )
+            
+            noise_pred = self.unet(latent_model_input,
+                                   t,
+                                   encoder_hidden_states=encoder_hidden_states
+                                  ).sample
+            
+            noise_pred_UC, noise_pred_C = noise_pred.chunk(2)
+            noise_pred = noise_pred_UC + gs * (noise_pred_C - noise_pred_UC)
+            
+            latents = self.scheduler.step(noise_pred, t, latents).prev_sample
+        
+        # decode
+        latents = latents / self.latent_scale
+        image = self.vae.decode(latents).sample
+        image = (0.5 * image + 0.5).clamp(0, 1)
+        image = image.detach().cpu().permute(0, 2, 3, 1).float().numpy()
+        image = (image * 255).round().astype("uint8")
+        image = [Image.fromarray(im) for im in image]
+        return image
+                                   
+            
+        
---
+++ smoke_test_gen.py
@@ -0,0 +1,12 @@
+from sd_model import SDModel
+import torch
+torch.set_grad_enabled(False)
+
+a = SDModel().cuda()
+im = a.generate('smoke test')[0]
+im.save('tmp_untrained.jpg')
+
+
+a = SDModel(pretrained_unet=True).cuda()
+im = a.generate('smoke test')[0]
+im.save('tmp_pretrained.jpg')
